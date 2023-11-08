e0aa16cfc11a975858a2337bfdf7cd2246e41e18
gianam <gianamnguyen023@gmail.com>
2023-11-08 16:08:42 +0700

    Initial commit

--- app/src/main/java/com/example/inventory/data/ItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -18,9 +18,7 @@ package com.example.inventory.data
 
 import kotlinx.coroutines.flow.Flow
 
-/**
- * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
- */
+
 interface ItemsRepository {
     /**
      * Retrieve all the items from the the given data source.
